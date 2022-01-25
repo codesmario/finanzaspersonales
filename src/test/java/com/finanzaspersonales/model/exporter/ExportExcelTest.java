39fcdb09ccd2acebce62fb693cb0eb4442ba3d8d
Denisse Lara <dmlaramartin@gmail.com>
2022-01-25 09:40:24 -0400

    Agrega setup a la prueba de excel

--- src/test/java/com/finanzaspersonales/model/exporter/ExportExcelTest.java
+++ src/test/java/com/finanzaspersonales/model/exporter/ExportExcelTest.java
@@ -1,17 +1,23 @@
 package com.finanzaspersonales.model.exporter;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
 class ExportExcelTest {
+    private ExportExcel exportExcel;
+
+    @BeforeEach
+    void setup() {
+        exportExcel = new ExportExcel();
+    }
 
     @Test
     @DisplayName("Check - Creating file")
     void exportYearFilenameTest() {
-        ExportExcel exportExcel = new ExportExcel();
         Assertions.assertDoesNotThrow(()-> {
             exportExcel.exportYear("testName.xls",2020);
         });
@@ -20,7 +26,6 @@ class ExportExcelTest {
     @Test
     @DisplayName("Check - Null or filename has no name")
     void exportYearNullNameTest() {
-        ExportExcel exportExcel = new ExportExcel();
         Assertions.assertDoesNotThrow(()-> {
             String filename = LocalDate.now() + ".xls";
             exportExcel.exportYear(filename,2020);
